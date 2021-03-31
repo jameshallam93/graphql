652cecaa81a75df654a5af9330988d537477cf6e
James <james.hallam93@gmail.com>
2021-03-31 21:24:31 +0100

    query saved in state and mapped to li

--- countriesgraphql/src/GraphQL/Queries.js
+++ countriesgraphql/src/GraphQL/Queries.js
@@ -3,6 +3,7 @@ import {gql} from "@apollo/client"
 export const LOAD_COUNTRIES = gql`
     query{
         countries{
+            code
             name
             continent{
                 name
